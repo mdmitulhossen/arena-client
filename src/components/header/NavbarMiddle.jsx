d81efbe59dcbd994f806500d47771502138c3327
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-12-30 21:46:33 +0600

    user menu done

--- src/components/header/NavbarMiddle.jsx
+++ src/components/header/NavbarMiddle.jsx
@@ -1,10 +1,12 @@
 import logo from '../../assets/logo/logo.png';
+import userAvater from '../../assets/user.png';
 import { IoIosSearch } from "react-icons/io";
 
 import { FaRegUser } from "react-icons/fa6";
 import { FaRegHeart } from "react-icons/fa";
 import { IoBagHandleOutline } from "react-icons/io5";
 const NavbarMiddle = () => {
+    const user = false
     return (
         <div className='border-b'>
             <div className='flex justify-between items-center containerArena py-1 gap-3'>
@@ -20,10 +22,38 @@ const NavbarMiddle = () => {
                         </button>
                     </div>
                 </div>
-                <div className='flex gap-4 items-center text-gray-600'>
-                    <button className=''>
-                        <FaRegUser size={30} />
-                    </button>
+                <div className='flex gap-4 items-center text-gray-600 '>
+                    {/* login user */}
+                    {
+                        user ?
+                            <button className='group relative'>
+                                <img src={userAvater} className='w-10 h-10 rounded-full' alt="" />
+
+                                {/* Open login user */}
+                                <div className="z-50 hidden group-hover:block absolute top-6 right-0  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                                    <div className="px-4 py-3">
+                                        <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                                        <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                                    </div>
+                                    <ul className="py-2" aria-labelledby="user-menu-button">
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </button>
+                            :
+                            <button className='bg-[#f18787] px-4 py-2 rounded text-sm font-semibold text-white hover:bg-[#b43e3e] duration-200'>Login</button>
+                    }
                     <button className='relative'>
                         <FaRegHeart size={30} />
                         <span className='bg-[#f18787] px-[4px] rounded-full text-xs font-bold text-white absolute -top-2 -right-2'>0</span>
@@ -32,6 +62,8 @@ const NavbarMiddle = () => {
                         <IoBagHandleOutline size={30} />
                         <span className='bg-[#f18787] px-[4px] rounded-full text-xs font-bold text-white absolute -top-1 -right-1'>0</span>
                     </button>
+
+
                 </div>
             </div>
         </div>
